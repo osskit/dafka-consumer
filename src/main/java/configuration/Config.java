78d502a94d7540e055f00676f7c53d1c5aab2d67
Guy Segal <guysegal.il@gmail.com>
2022-07-08 22:17:06 +0300

    remove unused imports and private setting file

--- src/main/java/configuration/Config.java
+++ src/main/java/configuration/Config.java
@@ -3,7 +3,6 @@ package configuration;
 import io.github.cdimascio.dotenv.Dotenv;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
