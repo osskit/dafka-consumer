db5248f75cf20b8c143edc97863a0d3d99369821
Omer Levi Hevroni <omerlh@users.noreply.github.com>
2023-05-07 13:06:42 +0300

    .

--- tests/testcontainers/kafka.ts
+++ tests/testcontainers/kafka.ts
@@ -1,9 +1,15 @@
-import {StartedNetwork} from 'testcontainers';
+import {StartedNetwork, Wait} from 'testcontainers';
 import {KafkaContainer} from 'testcontainers';
 import {Kafka, logLevel} from 'kafkajs';
 
 export const kafka = async (network: StartedNetwork) => {
-    const container = await new KafkaContainer().withNetwork(network).withNetworkAliases('kafka').start();
+    const container = await new KafkaContainer()
+        .withNetwork(network)
+        .withNetworkAliases('kafka')
+        .withWaitStrategy(
+            Wait.forLogMessage('Registered broker 1 at path /brokers/ids/1 with addresses: BROKER://kafka:9092')
+        )
+        .start();
     const client = new Kafka({
         logLevel: logLevel.NOTHING,
         brokers: [`${container.getHost()}:${container.getMappedPort(9093)}`],
