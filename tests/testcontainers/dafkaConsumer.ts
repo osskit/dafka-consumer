748bd6e51f23b024ce460b8fd9d47ab470fa8483
Guy Segal <guysegal.il@gmail.com>
2023-12-17 11:27:18 +0200

    .

--- tests/testcontainers/dafkaConsumer.ts
+++ tests/testcontainers/dafkaConsumer.ts
@@ -15,7 +15,7 @@ export const dafkaConsumer = async (
     const container = await new GenericContainer('bazel/src:image')
         .withExposedPorts(3000)
         .withNetwork(network)
-        .withEnvironment({...env, COMMIT_INTERVAL_MS: '100'})
+        .withEnvironment(env)
         .withWaitStrategy(Wait.forLogMessage('consumer was assigned to partitions'))
         .withStartupTimeout(startupTimeout)
         .start();
