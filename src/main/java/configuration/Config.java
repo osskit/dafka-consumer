7fb2f3cd489444ee45c13d4babcda411ffebe0e9
sarialalem1 <sarialalem@gmail.com>
2024-01-17 16:46:51 +0200

    .

--- src/main/java/configuration/Config.java
+++ src/main/java/configuration/Config.java
@@ -8,7 +8,6 @@ import org.apache.kafka.clients.consumer.StickyAssignor;
 
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
