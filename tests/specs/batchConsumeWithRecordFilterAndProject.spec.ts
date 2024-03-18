a5b31db5637413d204edb99fc38c14a0c57c9c07
Guy Segal <guysegal.il@gmail.com>
2024-03-18 19:28:34 +0200

    [minor] filter by record field

--- tests/specs/batchConsumeWithRecordFilterAndProject.spec.ts
+++ tests/specs/batchConsumeWithRecordFilterAndProject.spec.ts
@@ -1,13 +1,11 @@
 import type {Orchestrator} from '../testcontainers/orchestrator.js';
 import {start} from '../testcontainers/orchestrator.js';
 import {getCalls, mockHttpTarget} from '../services/target.js';
-import {getOffset} from '../services/getOffset.js';
 import {produce} from '../services/produce.js';
 import {topicRoutes} from '../services/topicRoutes.js';
-import {sortBy} from 'lodash-es';
 import delay from 'delay';
 
-describe('stream', () => {
+describe('tests', () => {
     let orchestrator: Orchestrator;
 
     beforeEach(async () => {
@@ -18,7 +16,10 @@ describe('stream', () => {
                 GROUP_ID: 'test',
                 TARGET_BASE_URL: 'http://mocks:8080',
                 TOPICS_ROUTES: topicRoutes([{topic: 'foo', targetPath: '/consume'}]),
-                RECORD_PICK_FIELD: 'data',
+                TARGET_PROCESS_TYPE: 'batch',
+                RECORD_FILTER_FIELD: 'type',
+                RECORD_FILTER_VALUE: 'created',
+                RECORD_PROJECT_FIELD: 'data',
             },
             ['foo']
         );
@@ -31,26 +32,20 @@ describe('stream', () => {
         await orchestrator.stop();
     });
 
-    it('consume with record pick field', async () => {
-        const target = await mockHttpTarget(orchestrator.wiremockClient, '/consume', 200);
+    it('batch consume with record filter and project', async () => {
+        const target1 = await mockHttpTarget(orchestrator.wiremockClient, '/consume', 200);
 
         await produce(orchestrator, {
             topic: 'foo',
-            messages: [{key: '1', value: JSON.stringify({data: 'foo1'})}],
-        });
-
-        await produce(orchestrator, {
-            topic: 'foo',
-            messages: [{key: '2', value: JSON.stringify({data: 'foo2'})}],
-        });
-
-        await produce(orchestrator, {
-            topic: 'foo',
-            messages: [{key: '3', value: JSON.stringify({data: 'foo3'})}],
+            messages: [
+                {key: '1', value: JSON.stringify({type: 'created', data: 'foo1'})},
+                {key: '2', value: JSON.stringify({type: 'created', data: 'foo2'})},
+                {key: '3', value: JSON.stringify({type: 'deleted', data: 'foo3'})},
+            ],
         });
 
         await delay(5000);
 
-        await expect(getCalls(orchestrator.wiremockClient, target)).resolves.toMatchSnapshot();
+        await expect(getCalls(orchestrator.wiremockClient, target1)).resolves.toMatchSnapshot();
     });
 });
