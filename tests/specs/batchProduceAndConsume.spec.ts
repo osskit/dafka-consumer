5278c3a1adf5e1653d7c50e1fa4d544266c1390c
Guy Segal <guysegal.il@gmail.com>
2024-04-04 11:05:20 +0300

    .

--- tests/specs/batchProduceAndConsume.spec.ts
+++ tests/specs/batchProduceAndConsume.spec.ts
@@ -71,7 +71,7 @@ describe('tests', () => {
             ],
         });
 
-        await delay(5000);
+        await delay(10000);
 
         await expect(
             getCalls(orchestrator.wiremockClient, target1).then((calls) => sortArray(calls))
